0d45a8f732bc8426af2bfb3961c83e418225562a
Philippe Beaudry <philippe.beaudry96@gmail.com>
2017-09-21 00:58:18 -0400

    Environment setup to get things started
    using XAMP, PHP with laravel framework, and composer, this as everything we need to build on

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome to Soen 343';
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        $title = 'This is the about page';
+        return view('pages.about')->with('title', $title);
+    }
+}
