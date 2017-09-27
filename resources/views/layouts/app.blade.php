bb48879c7f8a4728687513e1a234b3df3e7abd73
Raymart De Guzman <raymart54@gmail.com>
2017-09-27 16:06:45 -0400

    sidebar category for admin in progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,25 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel='stylesheet' href="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'soen343')}}</title>      
+        <title>{{config('app.name', 'soen343')}}</title>
     </head>
     <body>
         @include('inc.navbar')
         <div class='container'>
-            @include('inc.messages')
-            @yield('content')
+            <div class="row">
+
+
+                <div class="col-lg-3">
+                    @include('inc.sidebar')
+                </div>
+                <div class="col-lg-9">
+                    @include('inc.messages')
+                    @yield('content')
+                </div>
+
+            </div>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
     </body>
 </html>
