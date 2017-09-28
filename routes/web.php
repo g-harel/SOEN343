2df4f8e773c8182d55fd090d0304fad590f2c3af
Philippe Beaudry <philippe.beaudry96@gmail.com>
2017-09-28 18:17:26 -0400

    removed items from the nav menu

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,3 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/login', 'PagesController@login');
 
-Route::resource('items', 'ItemsController');
-
