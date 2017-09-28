1b19410f4a38dd430465fa3d2e9736a8b534391b
benadavid <benadavid_2007@hotmail.com>
2017-09-27 22:30:11 -0400

    adding radio buttons of type of electronics

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <a href="/items" class='btn btn-default'>Go Back</a>
     <h1>Create Items</h1>
-    {!! Form::open(['action' => 'ItemsController@store', 'method' => 'POST']) !!}
         <div class="form-group">
-            {{Form::label('brand', 'Brand')}}
-            {{Form::text('brand', '', ['class' => 'form-control', 'placeholder' => 'Brand'])}}
+            <h3 style="color:blue;">Type of Item</h1>
+            <form name ="form1" method ="post" action ="radioButton.php">            <input type="radio" name="type" value="computer"> Telivision<br>
+            <input type="radio" name="type" value="female"> Computer<br>
+            </form
         </div>
         <div class="form-group">
             {{Form::label('price', 'Price')}}
@@ -16,6 +17,7 @@
             {{Form::label('type', 'Type')}}
             {{Form::text('type', '', ['class' => 'form-control', 'placeholder' => 'TV, Computer, Monitor'])}}
         </div>
+        
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
 @endsection
