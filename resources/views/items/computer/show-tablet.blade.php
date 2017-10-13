fccc5c1cdfa257d81d6b467c094a0fa85e8ddeaa
omarhammoud <omar_hammoud96@hotmail.com>
2017-10-13 00:46:05 -0400

    modify tablet UI

--- resources/views/items/computer/show-tablet.blade.php
+++ resources/views/items/computer/show-tablet.blade.php
@@ -6,6 +6,7 @@
         <li>Computer</li>
         <li class="active">Tablet</li>
     </ol>
+<div class="col-lg-9">
     <p><button class="btn btn-success create-new-items">Add new</button></p>
     <table class="table table-bordered table-responsive bg-color-white">
         <thead>
@@ -50,10 +51,23 @@
             <td>--</td>
             <td>Yes</td>
             <td>No</td>
-            <td class="text-center"><p data-placement="top" data-toggle="tooltip" title="Edit"><a class="btn btn-primary btn-xs" href="#"><span class="fa fa-scissors"></span></a></p></td>
+            <td class="text-center"><p data-placement="top" data-toggle="tooltip" title="Edit"><a class="btn btn-primary btn-xs edit-tablet-link" href="" data-toggle="modal" data-target=".bs-edit-tablet-modal-lg"><span class="fa fa-scissors"></span></a></p></td>
             <td class="text-center"><p data-placement="top" data-toggle="tooltip" title="Delete"><a class="btn btn-danger btn-xs" ><span class="fa fa-trash"></span></a></p></td>
         </tr>
 
         </tbody>
     </table>
+</div>
+<div class="modal fade bs-edit-tablet-modal-lg" tabindex="-1" role="dialog" aria-labelledby="">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Editing Tablet</h4>
+                </div>
+                <div class="modal-body" id="edit-tablet-form-body">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
