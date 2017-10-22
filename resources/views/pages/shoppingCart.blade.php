abe47f9227f1cc3c0e5727b2ba567ad5ab071e80
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-10-22 12:21:35 -0400

    remove button now removes table row on click

--- resources/views/pages/shoppingCart.blade.php
+++ resources/views/pages/shoppingCart.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.btn').click(DeleteRow);
+    });
 
+    function DeleteRow()
+    { 
+        $(this).parents('tr').first().remove();
+    }
+</script>
 @section('content')
 <div class="container">
 <div class="row">
@@ -11,11 +21,11 @@
                     <th>Quantity</th>
                     <th class="text-center">Price</th>
                     <th class="text-center">Total</th>
-                    <th> </th>
+                    <th class="product-removal"> </th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr id="product-1">
                     <td class="col-sm-8 col-md-6">
                     <div class="media">
                         <a class="thumbnail pull-left" href="#"><i class="fa fa-laptop fa-5x"></i></a>
@@ -25,15 +35,18 @@
                         </div>
                     </div></td>
                     <td class="col-sm-1 col-md-1" style="text-align: center">
-                    <input type="email" class="form-control" id="exampleInputEmail1" value="2">
+                    <input type="number" class="form-control" id="exampleInputEmail1" value="2" min="1">
                     </td>
                     <td class="col-sm-1 col-md-1 text-center"><strong>$1000.00</strong></td>
                     <td class="col-sm-1 col-md-1 text-center"><strong>$2000.00</strong></td>
                     <td class="col-sm-1 col-md-1">
-                    <button type="button" class="btn btn-danger">
-                        <span class="glyphicon glyphicon-remove"></span> Remove
-                    </button></td>
-                </tr>
+                    <div class="product-removal">
+                        <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                        </button>
+                    </div>
+                    </td>
+                    </tr>
                 <tr>
                     <td class="cold-sm-8 col-md-6">
                     <div class="media">
@@ -44,14 +57,15 @@
                         </div>
                     </div></td>
                     <td class="col-md-1" style="text-align: center">
-                    <input type="email" class="form-control" id="exampleInputEmail1" value="1">
+                    <input type="number" class="form-control" id="exampleInputEmail1" min="1">
                     </td>
                     <td class="col-md-1 text-center"><strong>$2500.00</strong></td>
                     <td class="col-md-1 text-center"><strong>$2500.00</strong></td>
                     <td class="col-md-1">
-                    <button type="button" class="btn btn-danger">
-                        <span class="glyphicon glyphicon-remove"></span> Remove
-                    </button></td>
+                        <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Remove
+                        </button>
+                    </td>
                 </tr>
                 <tr>
                     <td>   </td>
