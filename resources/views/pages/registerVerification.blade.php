9f0856889d48eae4c12b5e8985271f16bd9a92a1
cynthiac3 <cynthia.cherrier@hotmail.com>
2017-10-23 20:44:38 -0400

    Add files via upload

--- resources/views/pages/registerVerification.blade.php
+++ resources/views/pages/registerVerification.blade.php
@@ -23,6 +23,20 @@ $postalCode = $_POST['postalCode'];
 $register = new Register($firstName,$lastName,$email,$password,$phoneNumber,$doorNumber,$street,$appt,$city,$province,$country,$postalCode);
 $result = $register->createUser();
 
+header("Location: http://" . $_SERVER['SERVER_NAME'] . "/login");
+
+/*alert("Inside registerVerification");
+
+if($result == 1){
+    $_SESSION['isAdmin'] = $result;
+    header("Location: http://" . $_SERVER['SERVER_NAME'] . "/admin");
+    exit();
+}
+else{
+    header("Location: http://" . $_SERVER['SERVER_NAME'] . "/login");
+    exit();
+}*/
+
 /*
 If registration is successful, login user 
 if($result == 1){
