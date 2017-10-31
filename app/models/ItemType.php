9d8a8a1e446d2b652e2546635f9994b0da3ceb40
Benjamin T <Benjamin.tanguay@gmail.com>
2017-10-31 17:17:31 -0400

    enum itemtype created

---
+++ app/models/ItemType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+abstract class ItemType {
+
+    const item = 0;
+    const monitor = 1;
+    const computer = 2;
+    const desktop = 3;
+    const laptop = 4;
+    const tablet = 5;
+}
