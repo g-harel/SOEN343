0472557ee5e75333d693fbd4cbd84c1b907707b2
Raymart De Guzman <raymart54@gmail.com>
2017-11-05 23:58:43 -0500

    added advanced search for items

--- resources/views/pages/viewTablet.blade.php
+++ resources/views/pages/viewTablet.blade.php
@@ -97,7 +97,39 @@
             <a href="/view/monitor" class="list-group-item">Monitor</a>
             <a href="/view/desktop" class="list-group-item">Desktop</a>
             <a href="/view/laptop" class="list-group-item">Laptop</a>
-            <a href="/view/tablet" class="list-group-item">Tablet</a>
+            <a href="/view/tablet" class="list-group-item active">Tablet</a>
+        </div>
+        <!-- advanced search -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Advanced Search</h3>
+            </div>
+            <div class="panel-body">
+                <form id="tablet-form" class="form-horizontal" action="" method="POST">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            Brand:
+                            <select required="" name="tablet-brand" id="tablet-brand" class="form-control">
+                                <option title="Select brands" value="">Select brands</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            Hard Drive Size (GB):
+                            <select required="" name="tablet-storage-capacity" id="tablet-storage-capacity" class="form-control">
+                                <option title="Select storage qty" value="">Select storage size</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            Price: <br>
+                            min:<input type="number" min="1" step="0.01" placeholder="0.00" max="99999" name="tablet-price" id="laptop-price" class="form-control">
+                            max:<input type="number" min="1" step="0.01" placeholder="0.00" max="99999" name="tablet-price" id="laptop-price" class="form-control" >
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-sm" name="search-tablet-form" id="search-tablet-form">Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div><!--/.sidebar-offcanvas-->
 </div>
