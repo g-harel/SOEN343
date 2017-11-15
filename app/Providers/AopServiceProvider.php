88b0bcf2b0ecbf87941a0ae9d3fd602291817b97
benadavid <benadavid_2007@hotmail.com>
2017-11-15 08:39:42 -0500

    create logging aspect example

--- app/Providers/AopServiceProvider.php
+++ app/Providers/AopServiceProvider.php
@@ -23,6 +23,10 @@ class AopServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(LoggingAspect::class, function (Application $app) {
+            return new LoggingAspect($app->make(LoggerInterface::class));
+        });
+
+        $this->app->tag([LoggingAspect::class], 'goaop.aspect');
     }
 }
