4d7fa2a8fc7609123acc628d27c0680925197b7b
Raymart De Guzman <raymart54@gmail.com>
2017-11-15 16:38:12 -0500

    fix view/profile link

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,7 +3,6 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/">{{config('app.name', 'soen343')}}</a>
         </div>
-
         <ul class="nav navbar-nav">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About </a></li>
@@ -36,7 +35,7 @@
                             <span class="glyphicon glyphicon-shopping-cart"></span>
                         </a>
                     </li>
-                    <li><a href="/views/profile">Audrey Hunt</a></li>
+                    <li><a href="/view/profile">Audrey Hunt</a></li>
                 @endif
             @endif
             <li><a href="/logout">Logout</a></li>
