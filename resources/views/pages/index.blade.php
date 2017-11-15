8251f377b5a3663394524ffa4215ca3db4a7b317
Ishmam Murtaza <ishmammurtaza@gmail.com>
2017-11-15 17:42:44 -0500

    minor commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('page-title')
-    Welcome to Soen 343
+    Welcome to iShop Electronics
 @endsection
 @section('content')
     <div class='jumbotron text-center'>
-        <h1>Welcome to 343 Electronics</h1>
+        <h1>Welcome to iShop Electronics</h1>
         <p>Please Log in to use the Shopping cart. Otherwise, feel free to browse out products! </p>
         <p><a class='btn btn-primary btn-lg' href='/login' role='button'>Login</a> <a class='btn btn-success btn-lg' href='/register' role='button'>Register</a>
     </div>
