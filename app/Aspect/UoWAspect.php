4edbc7fffbb1a3e7e6c40a424e0ecfe86f0402a4
benadavid <benadavid_2007@hotmail.com>
2017-11-16 09:10:26 -0500

    create aspect class for uow

---
+++ app/Aspect/UoWAspect.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+/*
+ * Go! AOP framework
+ */
+use Go\Aop\Aspect;
+use Go\Aop\Intercept\MethodInvocation;
+use Go\Lang\Annotation\After;
+use Go\Lang\Annotation\Before;
+use Go\Lang\Annotation\Pointcut;
+use Go\Core\AspectContainer;
+use Go\Core\AspectKernel;
+/**
+ * Aspect of Unit of Work
+ */
+class UoWAspect implements Aspect
+{
+    /**
+     * Method that advices to activate the UoW after committing to Mapper
+     *
+     * @param MethodInvocation $invocation Invocation
+     * @After("execution(public **->*(*))")
+     */
+    protected function SaveAfterCommit(MethodInvocation $invocation)
+    {
+        $this->
+    }
+}
