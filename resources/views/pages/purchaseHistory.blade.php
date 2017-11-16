601d470b92ff87bf119bce79a3ccad7fc0cfd0a7
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-11-15 21:46:27 -0500

    Main UI for purchase history done with hard coded item.

--- resources/views/pages/purchaseHistory.blade.php
+++ resources/views/pages/purchaseHistory.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.app')
 @section('content')
-    <div class='jumbotron text-center'>
-        <h1>Purchase History</h1>
-        <p></p>
-        <h1>Returns</h1>
+    <div class='jumbotron'>
+        <h3>Purchase History</h3>
+        <table>
+            <tr>
+                <th>Product</th>
+                <th>Serial Number</th>
+                <td></td>
+            </tr>
+            <tr>
+                <td>item</td>
+                <td><a class="btn" href="#">Return</a>
+            </tr>
+        </table>
+        <hr>
+        <h3>Returns</h3>
+        <table>
+            <tr>
+                <th>Product</th>
+                <th>Serial Number</th>
+            </tr>
+            <tr>
+                <td>item</td>
+            </tr>
+        </table>
     </div>
 @endsection
