9bf730e28f7d5a5950cff674660b99ba8bf25438
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-11-16 16:53:55 -0500

    formatting

--- resources/views/pages/purchaseHistory.blade.php
+++ resources/views/pages/purchaseHistory.blade.php
@@ -6,28 +6,28 @@
             @if(empty($units))
                 <p>Your purchase history is empty.</p>
             <!-- still need to display purchase name -->
-                @else
-                    <table>
-                        <tr>
-                            <th>Product</th>
-                            <th>Serial Number</th>
-                            <td></td>
-                        </tr>
-                    @foreach($units as $unit)
-                        <tr>
-                            <td>{{ $unit['name'] }}, {{ $unit['serial'] }},{{ $unit['price'] }}$, purchased on {{ $unit['purchased_date'] }}</td> 
-                            <td>
-                                <form action="/returnPurchase" method="post">
-                                {{ csrf_field() }}
-                                    <input type="hidden" name="transaction-id" id="transaction-id" value="{{ $unit['id'] }}" />
-                                    <input type="hidden" name="serial-nb" id="serial-nb" value="{{ $unit['serial'] }}" />   
-                                    <button type="submit" name="submit" class="btn btn-default">Return</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                @endif
+            @else
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th>Serial Number</th>
+                        <td></td>
+                    </tr>
+                @foreach($units as $unit)
+                    <tr>
+                        <td>{{ $unit['name'] }}, {{ $unit['serial'] }},{{ $unit['price'] }}$, purchased on {{ $unit['purchased_date'] }}</td> 
+                        <td>
+                            <form action="/returnPurchase" method="post">
+                            {{ csrf_field() }}
+                                <input type="hidden" name="transaction-id" id="transaction-id" value="{{ $unit['id'] }}" />
+                                <input type="hidden" name="serial-nb" id="serial-nb" value="{{ $unit['serial'] }}" />   
+                                <button type="submit" name="submit" class="btn btn-default">Return</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
             @endif
+        @endif
         </table>
     </div>
 @endsection
