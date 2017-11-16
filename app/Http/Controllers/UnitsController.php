610d5887ebc77aa064e044fecbb15f53e1a0dcd0
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-11-16 11:04:03 -0500

    Implemented show purchase function display purchases linked to account id

--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -2,15 +2,16 @@
 namespace App\Http\Controllers;
 
 use App\Mappers\UnitMapper;
+use App\Mappers\AccountMapper;
 
 class UnitsController extends Controllers {
 
     public function showPurchase(){
-
+        return view('purchaseHistory', ['units' => UnitMapper::getPurchased(getId())]);
     }
 
     public function returnPurchase(){
-        
+
     }
 }
 ?>
