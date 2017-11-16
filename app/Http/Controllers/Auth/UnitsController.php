9b1c43ac2eb6cfa838e9aef33ebb5c3a2671328d
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-11-16 10:29:49 -0500

    Created UnitsController

---
+++ app/Http/Controllers/Auth/UnitsController.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Http\Controllers;
+
+use App\Mappers\UnitMapper;
+
+class UnitsController extends Controllers {
+
+    public function showPurchase(){
+
+    }
+
+    public function returnPurchase(){
+        
+    }
+}
+?>
