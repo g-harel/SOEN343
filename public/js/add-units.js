e2cc8dbb5024de3e894b7ba13fc1946112ed2734
Raymart De Guzman <raymart54@gmail.com>
2017-11-16 18:01:59 -0500

    all js for unit modal is done

--- public/js/add-units.js
+++ public/js/add-units.js
@@ -1,22 +1,69 @@
 $(document).ready(() => {
     const formUnits = {
         unitForm: $('#serial-next'),
+        hideNext: function() {
+            formUnits.unitForm.removeClass('hidden');
+        },
+        displayNext: function () {
+            formUnits.unitForm.addClass('hidden');
+        },
         monitor: {
             form: $('form#monitor-form-units'),
             unitInput: $('#monitor-units'),
-            modal: '.addUnitsMonitorLink'
+            modal: '.addUnitsMonitorLink',
+            emptyVal: function () {
+                formUnits.monitor.form.find('#units-inputs-container').empty();
+                formUnits.monitor.unitInput.val(0);
+                formUnits.hideNext();
+            }
+        },
+        desktop: {
+            form: $('form#desktop-form-units'),
+            unitInput: $('#desktop-units'),
+            modal: '.addUnitsDesktopLink',
+            emptyVal: function () {
+                formUnits.desktop.form.find('#units-inputs-container').empty();
+                formUnits.desktop.unitInput.val(0);
+                formUnits.hideNext();
+            }
+        },
+        laptop: {
+            form: $('form#laptop-form-units'),
+            unitInput: $('#laptop-units'),
+            modal: '.addUnitsLaptopLink',
+            emptyVal: function () {
+                formUnits.laptop.form.find('#units-inputs-container').empty();
+                formUnits.laptop.unitInput.val(0);
+                formUnits.hideNext();
+            }
+        },
+        tablet: {
+            form: $('form#tablet-form-units'),
+            unitInput: $('#tablet-units'),
+            modal: '.addUnitsTabletLink',
+            emptyVal: function () {
+                formUnits.tablet.form.find('#units-inputs-container').empty();
+                formUnits.tablet.unitInput.val(0);
+                formUnits.hideNext();
+            }
         }
     };
     $(".modal"+formUnits.monitor.modal+"").on("hidden.bs.modal", function() {
-        formUnits.monitor.form.find('#units-inputs-container').empty();
-        formUnits.monitor.unitInput.val(0);
-        formUnits.unitForm.removeClass('hidden');
+        formUnits.monitor.emptyVal();
+    });
+    $(".modal"+formUnits.desktop.modal+"").on("hidden.bs.modal", function() {
+        formUnits.desktop.emptyVal();
+    });
+    $(".modal"+formUnits.laptop.modal+"").on("hidden.bs.modal", function() {
+        formUnits.laptop.emptyVal();
+    });
+    $(".modal"+formUnits.tablet.modal+"").on("hidden.bs.modal", function() {
+        formUnits.tablet.emptyVal();
     });
     $('.generate-serial-form').on('click', function () {
         let fields = null;
         let appendElem = null;
         if($(this).attr('name', 'monitor-serial')) {
-            console.log('hello');
             fields = formUnits.monitor.unitInput.val();
             appendElem = function(h) {
                 formUnits.monitor.form.find('#units-inputs-container').append(h);
@@ -28,7 +75,7 @@ $(document).ready(() => {
                 <input type="text" id="serial-number" value="${randomString()}" class="form-control">
                 </div>`)
         }
-        formUnits.unitForm.addClass('hidden');
+        formUnits.displayNext();
     })
 });
 
