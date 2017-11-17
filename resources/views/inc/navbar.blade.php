064c31d389c4e2e73c4f00972bc2c2393b05fab7
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2017-11-17 10:47:00 -0500

    Updated navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,44 +1,45 @@
 <nav class="navbar navbar-inverse" style="border-radius: 0;">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/">{{config('app.name', 'soen343')}}</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About </a></li>
-            @if(isset($_SESSION) && !empty($_SESSION))
-                @if($_SESSION['isAdmin'] == 1)
-                    <li><a href="/items">Items</a></li>
-                @endif
+<div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="/">{{config('app.name', 'soen343')}}</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About </a></li>
+        @if(isset($_SESSION) && !empty($_SESSION))
+            @if($_SESSION['isAdmin'] == 1)
+                <li><a href="/items">Items</a></li>
             @endif
-            @if(isset($_SESSION)  && !empty($_SESSION))
-                @if($_SESSION['isAdmin'] != 1)
-                    <li><a href="/view">Products</a></li>
-                @endif
-            @endif
-            @if(empty($_SESSION))
+        @endif
+        @if(isset($_SESSION)  && !empty($_SESSION))
+            @if($_SESSION['isAdmin'] != 1)
                 <li><a href="/view">Products</a></li>
+                <li><a href="/purchaseHistory">Purchase History</a></li>
             @endif
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            @if(!(isset($_SESSION))  || empty($_SESSION))
-                <li><a href="/register">Sign Up</a></li>
-                <li><a href="/login">Login</a></li>
+        @endif
+        @if(empty($_SESSION))
+            <li><a href="/view">Products</a></li>
+        @endif
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        @if(!(isset($_SESSION))  || empty($_SESSION))
+            <li><a href="/register">Sign Up</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/shoppingCart">
+                    <span class="glyphicon glyphicon-shopping-cart"></span>
+                </a>
+            </li>
+        @endif
+        @if(isset($_SESSION) && !empty($_SESSION))
+            @if($_SESSION['isAdmin'] != 1)
                 <li><a href="/shoppingCart">
                         <span class="glyphicon glyphicon-shopping-cart"></span>
                     </a>
                 </li>
+                <li><a href="/view/profile">View Profile</a></li>
             @endif
-            @if(isset($_SESSION) && !empty($_SESSION))
-                @if($_SESSION['isAdmin'] != 1)
-                    <li><a href="/shoppingCart">
-                            <span class="glyphicon glyphicon-shopping-cart"></span>
-                        </a>
-                    </li>
-                    <li><a href="/view/profile">View Profile</a></li>
-                @endif
-                  <li><a href="/logout">Logout</a></li>
-            @endif
-        </ul>
-    </div>
+            <li><a href="/logout">Logout</a></li>
+        @endif
+    </ul>
+</div>
 </nav>
