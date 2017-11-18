ce7da4d54cce12f3c07d45f5701ea29a88b34b1b
omarhammoudgit config user.name omarhammoud <omar_hammoud96@hotmail.com>
2017-11-17 20:07:37 -0500

    small fix

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Route::get('/logout', 'PagesController@logout');
 Route::get('/register', 'PagesController@register');
 Route::post('registerUser', 'PagesController@registerUser');
 Route::get('/shoppingCart', 'PagesController@shoppingCart');
-Route::post('/deleteAccount', 'PagesController@deleteAccount');
+if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] != 1) {
+    Route::post('/deleteAccount', 'PagesController@deleteAccount');
+}
 Route::get('/purchaseHistory', 'PagesController@purchaseHistory');
 
 Route::get('showPurchase', 'UnitsController@showPurchase');
